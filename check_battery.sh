ade4a9efd377f9f0415ae2c49b4587b9f39ea865
Florian Guilbaud <florian.guilbaud.work@gmail.com>
2018-04-26 23:45:02 +0200

    Add status «full battery» and the notification, update i3config

--- check_battery.sh
+++ check_battery.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+kill -15 $(cat ($pwd)/.pid)
+
 while true
 do
 	battery_level=$(echo $(acpi -b | cut -d',' -f2 |  tr -dc "0-9"))
@@ -8,6 +10,10 @@ do
 	if [ $battery_level -le 20 ] && [ $charge_or_discharge = "D" ]; then
 		i3-nagbar -t error -m 'I WILL DIE SOON.......'
 	fi
+
+    if [ $battery_level -eq 100 ] && [ $charge_or_discharge != "D" ]; then
+        i3-nagbar -t warning -m 'I AM FULL UNPLUG ME'
+    fi
 
 	sleep 300 # 300 seconds or 5 minutes
 done
